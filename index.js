3fb304beac93048042ec0fbf44fa76a4a05e858f
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-08-31 13:50:39 -0700

    adding changes for deployment

--- index.js
+++ index.js
@@ -199,6 +199,7 @@ this.sheetsService.spreadsheets.values.append({
 
 
 // listen on port 3000
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("You're listening to the smooth sounds of port 3000 in the morning.");
 });
+
